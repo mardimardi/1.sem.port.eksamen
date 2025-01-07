82d2d3ab5051e1713b6f2851506a8268ac089ba6
mardimardi <caho0004@stud.kea.dk>
2025-01-07 15:08:14 +0100

    header

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,17 +1,17 @@
+
 const burger = document.querySelector(".burger");
 const nav = document.querySelector("nav");
-const menu = document.querySelector(".menu");
 
 burger.addEventListener("click", burgerClick);
 
 function burgerClick() {
-  burger.classList.toggle("active");
+  burger.classList.toggle("active"); 
   nav.classList.toggle("active");
 }
 
 nav.addEventListener("click", menuClick);
 
 function menuClick() {
-  burger.classList.remove("active");
-  nav.classList.remove("active");
+  burger.classList.remove("active"); 
+  nav.classList.remove("active"); 
 }
